b9deeee0cdf9cceeeb629ded5c97538e81980c00
wonjr <41326736+wonjr@users.noreply.github.com>
2020-08-29 13:37:19 +0900

    Add files via upload

--- satclass.py
+++ satclass.py
@@ -6,8 +6,8 @@ c = "abc"
 
 print(a)
 print(type(a))
-a = "3"
 
+a = "3"
 print(a, type(a))
 
 a = int(a)
@@ -15,7 +15,6 @@ print(a, type(a))
 
 a = float(a)
 print(a, type(a))
-
 """
 
 """
@@ -43,8 +42,9 @@ a = int(input())
 
 if a % 2 == 0:
     print("even")
-if a % 2 == 1:
+else:
     print("odd")
+
 """
 """
 a = int(input())
@@ -53,8 +53,36 @@ if a == 0:
     print("zero")
 elif a == 1:
     print("one")
+elif a == 2:
+    print("two")
 else:
     print("etc")
+"""
+"""
+a = 5
+b = 3
+c = int(input())
+
+if c == 1:
+    print(a+b)
+elif c == 2:
+    print(a - b)
+elif c == 3:
+    print(a * b)
+elif c == 4:
+    print(a / b)
+elif c == 5:
+    print(a % b)
+"""
+"""
+# 1.초기값 2. 조건식 3. 증가/감소
+a = 0
+while a < 100:
+    print("10")
+    a += 1 # a = a + 1
+
+for i in range(101):
+    print("10")
 """
 """
 for i in range(1,10,3):
@@ -68,6 +96,7 @@ for i in range(1, 11):
 for i in range(10, 0, -1):
     print("*" * i)
 """
+
 """
 for i in range(1,100):
     one = i % 10
@@ -90,7 +119,13 @@ for i in range(1, 100):
     else:
         print(i)
 """
-
+"""
+for a in range(2,10):
+    for i in range(1,10):
+        print("{} * {} = {}".format(a, i, a*i))
+    print()
+#------------------------------------------------
+"""
 """
 for i in range(2,10):
     for j in range(1,10):
@@ -177,7 +212,7 @@ while x != 0:
 for i in range(len(a)-1, -1, -1):
     print(a[i], end=" ")
 """
-
+"""
 b = int(input())
 a = []
 for i in range(1, 101):
@@ -196,7 +231,7 @@ while low <= high:
     else:
         low = mid + 1
         cnt += 1
-
+"""
 """
 a = [1, 2, 3, 4]
 b = []
@@ -212,3 +247,89 @@ a = [1, 2, 3, 4, 5, 6, 7]
 print(a[2:5])
 """
 
+"""
+start = 0
+end = 100
+mid = (start + end) // 2
+val = int(input())
+for i in range(100):
+    if val == mid:
+        break
+    elif val > mid:
+        start = mid
+    else:
+        end = mid
+    mid = (start + end) // 2
+if i > 50:
+    print("not find value")
+else:
+    print("find",i)
+"""
+"""
+a = input()
+
+for i in range(0, len(a)//2):
+    if a[i] != a[len(a)-1-i]:
+        print("NO")
+        exit()
+print("YES")
+"""
+"""
+a = input()
+reverse = []
+not_reverse = []
+for i in range(len(a)):
+    reverse.append(a[i])
+    not_reverse.append(a[len(a)-1-i])
+flag=0
+for i in range(len(reverse)):
+    if reverse[i] != not_reverse[i]:
+        flag = 1
+        break
+if flag == 1:
+    print("not same")
+else:
+    print("same")
+"""
+"""
+a = input()
+flag = 0
+for i in range(len(a)):
+    if a[i] != a[len(a)-1-i]:
+        flag = 1
+        break
+if flag == 1:
+    print("not same")
+else:
+    print("same")
+"""
+
+"""
+#튜플
+a = (1,2,3)
+
+#한번 값을 한단하면 바꿀 수 없다. list와 비슷하지만 메모리 측면에서 더 효율적으로 사용할 수도 있다.
+"""
+
+"""
+a = [1,"a",[1,"a"]]
+a = [[1,2,3],[4,5,6]]
+print(a[0])
+print(a[0][2])
+"""
+""" = {}
+a = dict()
+
+a = {'kkk': 170, 'age':100, 'value': 200, 'height' : 180, 'height': 190}
+print(a['kkk'])
+a.keys()
+print(a['height'])
+"""
+
+def add_test(x, y):
+    #print(x + y)
+    return x + y
+#add_test(1,3)
+print(add_test(1,3))
+c = add_test(1,3)
+print(c)
